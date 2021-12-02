1d89baa3305d4e2ffa80ecfaca07b780a30c1ecb
Jesper Brown <jesper.brown@gmail.com>
2021-12-02 10:44:49 +0100

    Day2B finished

--- day2/day2b.py
+++ day2/day2b.py
@@ -1,22 +1,20 @@
 commands = None
-x, y = (0, 0)
+horizontal, depth, aim = (0, 0, 0)
 
 with open('./input.txt', 'r') as fio:
     commands = fio.readlines()
 
 commands = [x.strip('\n') for x in commands]
 
-print(x)
-print(y)
-
 for command in commands:
     match command.split():
         case ["forward", v]:
-            x += int(v)
+            horizontal += int(v)
+            depth += aim * int(v)
         case ["down", v]:
-            y += int(v)
+            aim += int(v)
         case ["up", v]:
-            y -= int(v)
+            aim -= int(v)
 
-print(x*y)
+print(horizontal * depth)
 
