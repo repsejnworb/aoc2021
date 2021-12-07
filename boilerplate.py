1fa25645c46fb6a76e4dc3c40e484c2a0b901ecd
Jesper Brown <jesper.brown@gmail.com>
2021-12-07 22:10:05 +0100

    forgot the reading in boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -6,5 +6,9 @@ def main():
     parser.add_argument("--part2", action="store_true")
     args = parser.parse_args()
 
+    with open(f"./{args.inputfile}", 'r') as fio:
+        input_lines = fio.read().splitlines()
+    print(input_lines)
+
 if __name__ == "__main__":
     main()
