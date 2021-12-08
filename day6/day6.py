21760267a049c1c79989f2c39a3ab4bcdd561680
Jesper Brown <jesper.brown@gmail.com>
2021-12-08 15:08:42 +0100

    day6: cleaned out old unused code

--- day6/day6.py
+++ day6/day6.py
@@ -1,45 +1,6 @@
 import argparse
 from collections import Counter
 
-def tick(fishes):
-    spawned = []
-    for index, fish in enumerate(fishes):
-        if fish == 0:
-            fishes[index] = 6
-            spawned.append(8)
-        else:
-            fishes[index] -= 1
-    fishes.extend(spawned)
-
-
-
-""" 
-Idea? 
-Just count with a list instead. 
-Index is the state (0-8), and value is number of fishes in that state.
-Then to get a tick update you pop(0) to remove all zeroes and shift
-the numbers in one go.
-Then add the value that got popped to index(6) and make a new element
-of the value at the end of the list (index(8))
-"""
-
-def old_solution():
-    # setup zeroed counters for each possible state
-    fishes = new_counter()
-
-    # handle input
-    fishes.update(input_data)
-
-    print(f"Simulating {args.days} days")
-    for day in range(1, args.days + 1):
-        to_spawn = fishes[0]
-        state = new_counter()
-        for key, value in fishes.items():
-            if key == 0:
-                state[6] += value
-
-            state[key] += value
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("inputfile")
@@ -49,7 +10,6 @@ def main():
     args = parser.parse_args()
 
     with open(f"./{args.inputfile}", 'r') as fio:
-        # prepare data
         input_data = fio.read().strip()
         input_data = [int(x) for x in input_data.split(',')]
 
