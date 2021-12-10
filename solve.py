ca35ba3b32ba2fcab1db04aba95bdc2507f9eee3
Jesper Brown <jesper.brown@gmail.com>
2021-12-10 15:10:42 +0100

    solve.py: backing up

--- solve.py
+++ solve.py
@@ -5,6 +5,7 @@ from itertools import filterfalse
 import sys
 from pathlib import Path
 
+
 SOLUTION_INTERFACE = {
     "variables": [
         "SAMPLE_PART1",
@@ -19,6 +20,7 @@ SOLUTION_INTERFACE = {
         }
     ]
 }
+
 
 def verify_implemented(solution):
     """ Verifies that a solution file implements expected variables
@@ -38,9 +40,6 @@ def verify_implemented(solution):
     return implemented, result
 
 
-    #signature = inspect.signature()
-
-
 def get_file_paths(input_location):
     paths = {
         "directory": Path(input_location),
@@ -53,34 +52,27 @@ def get_file_paths(input_location):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("solution_location")
+    parser.add_argument("solution")
     parser.add_argument("--sample", action="store_true")
     parser.add_argument("--part2", action="store_true")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
 
-    paths = get_file_paths(args.solution_location)
+    solution_name = args.solution
+
+    paths = get_file_paths(solution_name)
     if missing_files := list(filterfalse(lambda p: p.exists(), paths.values())):
         for missing in missing_files:
             print(f"Couldn't find required file: {missing}")
         sys.exit(1)
 
-    solution_folder = Path(args.solution_location)
-    puzzle_input_path = solution_folder / "input.txt"
-    sample_input_path = solution_folder / "sample.txt"
-
     try:
-        #solution = importlib.import_module(args.day)
-        solution = SourceFileLoader("kiss", str(solution_location))
-        print(solution.is_package("kiss"))
+        solution = SourceFileLoader(solution_name, str(paths["solution"])).load_module()
     except ModuleNotFoundError as e:
         print(f"Couldn't find a solution named '{args.day}''")
         sys.exit(1)
 
-    breakpoint()
-
     implemented, result = verify_implemented(solution)
-    breakpoint()
     if not implemented:
         print(f"Solution '{solution}' has not implemented:")
         for expected_attr, _ in filterfalse(lambda r: r[1], result.items()):
@@ -88,17 +80,10 @@ def main():
             print(f"'{expected_attr}' not implemented.")
         sys.exit(1)
 
-
-    #solution_directory = Path(solution.__file__).parent
-    #puzzle_input_path = solution_directory / "input.txt"
-    #sample_input_path = solution_directory / "sample.txt"
-
-
     sample_answer = solution.SAMPLE_PART1 if args.part2 else solution.SAMPLE_PART2
     known_answer = solution.SOLVED_PART2 if args.part2 else solution.SOLVED_PART2
 
-    input_path, expected_answer = (sample_input_path, sample_answer) if args.sample else (puzzle_input_path, known_answer)
-    print(f"Running configuration '{input_file}' expecting answer '{expected_answer}'")
+    input_path, expected_answer = (paths["sample_input"], sample_answer) if args.sample else (paths["puzzle_input"], known_answer)
 
     with input_path.open() as fio:
         data = fio.read()
