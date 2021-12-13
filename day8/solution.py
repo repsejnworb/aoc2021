0eba4de64c6f92f061cf3c9e4258afb06f52cdd9
Jesper Brown <jesper.brown@gmail.com>
2021-12-13 13:57:23 +0100

    day8: part1 done

--- day8/solution.py
+++ day8/solution.py
@@ -1,19 +1,85 @@
+import itertools
+import pathlib
 import unittest
+
+""" 
+OG RENDERING:
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+
+"""
+
+DIGIT_MAP = {
+    0: "abcefg",
+    1: "cf",
+    2: "acdeg",
+    3: "acdfg",
+    4: "bcdf",
+    5: "abdfg",
+    6: "abdefg",
+    7: "acf",
+    8: "abcdefg",
+    9: "abcdfg",
+}
+
+
+def lookup_unique_digit(code: str):
+    matches = [k for k,v in DIGIT_MAP.items() if len(v) == len(code)]
+    if len(matches) > 1:
+        return None
+    else:
+        return matches[0]
 
 
 def solve_puzzle(input: str):
-    return "SAMPLE_ANSWER"
+    lines = input.splitlines()
+    digits = []
+    for line in lines:
+        signal_patterns, digit_output = [v.split() for v in line.split('|')]
+        for code in digit_output:
+            digit = lookup_unique_digit(code)
+            if digit is not None:
+                digits.append(digit)
+    print(digits)
+    return len(digits)
 
 
 def main():
-    with open('./input.txt') as fio:
+    puzzle_input_path = pathlib.Path(__file__).parent.resolve() / "input.txt"
+    with puzzle_input_path.open() as fio:
         print(f"Answer is: {solve_puzzle(fio.read())}")
 
 
 class TestSolution(unittest.TestCase):
-    SAMPLE = "SAMPLEDATA\n"
+    _SAMPLE = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf\n"
+    SAMPLE = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
     def test_part1(self):
-        self.assertEqual(solve_puzzle(self.SAMPLE), 'SAMPLE_ANSWER')
+        self.assertEqual(solve_puzzle(self.SAMPLE), 26)
     def test_part2(self):
         self.assertEqual(solve_puzzle(self.SAMPLE), 'SAMPLE_ANSWER')
 
